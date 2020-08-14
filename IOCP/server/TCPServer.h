ca4b330af4370cdb3b276f5c7271c7e8cb21babc
sjw <949629921@qq.com>
2020-08-14 16:07:40 +0800

    临时提交

---
+++ IOCP/server/TCPServer.h
@@ -0,0 +1,24 @@
+// Copyright (c) 2020 by Bug
+//
+// file  : TCPServer.h
+// since : 2020-8-14
+// desc  : windowsÍøÂç±à³Ì- IOCP tcp_server
+
+#ifndef __TCP_SERVER__
+#define __TCP_SERVER__
+
+#include <iostream>
+
+class TCPServer
+{
+public:
+	TCPServer();
+	~TCPServer();
+};
+
+#endif // !__TCP_SERVER__
+
+
+
+
+
--- IOCP/server/server.cpp
+++ IOCP/server/server.cpp
@@ -1,10 +1,15 @@
 ﻿// server.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
-#include <iostream>
+
+#include "TCPServer.h"
 
 int main()
 {
     std::cout << "IOCP server!\n";
+
+	TCPServer server;
+
+	return 0;
 }
 
